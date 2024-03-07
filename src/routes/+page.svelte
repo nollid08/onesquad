5367527e719d668acace91ec3e055a3beac55dcc
nollid08 <dillonkdy@gmail.com>
2024-03-07 22:28:15 +0000

    Added DaisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
-<h1 class="text-3xl font-bold underline">Landing Page</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<div class="h-full">
+	<div class="navbar bg-base-100">
+		<div class="flex-1">
+			<a class="btn btn-ghost text-xl" href="/">OneSquad</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal px-1">
+				<li><a href="/login">Login</a></li>
+			</ul>
+			<ul class="menu menu-horizontal px-1">
+				<li><a href="/login">Sign Up!</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="grid place-items-center h-72">
+		<h1 class="text-3xl font-bold underline">Landing Page</h1>
+	</div>
+</div>
